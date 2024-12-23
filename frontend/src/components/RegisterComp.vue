cb8b88fbc0eb3ba830f1625d165a15480bf288fc
Maicon Gavino <maiconserrao57@gmail.com>
2024-12-23 17:14:40 -0400

    v1

--- frontend/src/components/RegisterComp.vue
+++ frontend/src/components/RegisterComp.vue
@@ -46,6 +46,18 @@ export default {
           />
         </div>
         <div class="input-container">
+          <label class="register-label" for="phone">Telefone</label>
+          <input
+            type="tel"
+            name="phone"
+            id="phone"
+            v-model="form.phone"
+            placeholder="Seu Telefone..."
+            required
+            class="register-input"
+          />
+        </div>
+        <div class="input-container">
           <label class="register-label" for="email">Email</label>
           <input
               type="email"
